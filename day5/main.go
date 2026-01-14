83eb2c4ad1afb24fe9d2aa30654f05f5e8970eb0
bzzs10 <bzzs80368@gmail.com>
2026-01-15 00:02:25 +0300

    Add files via upload

--- day5/main.go
+++ day5/main.go
@@ -1,15 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type User struct {
+	Name       string
+	Age        int
+	PhoneNuber string
+	IsClose    bool
+	Rating     float64
+}
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		fmt.Println(i, "hello world")
-
-		if i%2 == 0 {
-			fmt.Println("четное")
-		} else {
-			fmt.Println("не четное")
-		}
+	user := User{
+		Name:       "bob",
+		Age:        25,
+		PhoneNuber: "+9(545)210-23-03",
+		IsClose:    true,
+		Rating:     3.4,
 	}
+
+	hello(user)
+	fmt.Println()
+
+	fmt.Println("full-user:", user)
+
+	fmt.Println("name:", user.Name)
+	fmt.Println("Age:", user.Age)
+	fmt.Println("PhoneNuber:", user.PhoneNuber)
+	fmt.Println("IsClose:", user.IsClose)
+	fmt.Println("IsClose:", user.Rating)
+
+	fmt.Println("--------------------")
+
+	fmt.Println("full-user:", user)
+
+	user.Name = "kitty"
+	fmt.Println("name:", user.Name)
+	user.Age = 900
+	fmt.Println("Age:", user.Age)
+	user.PhoneNuber = "+1(909)09-09-999"
+	fmt.Println("PhoneNuber:", user.PhoneNuber)
+	user.IsClose = false
+	fmt.Println("IsClose:", user.IsClose)
+	user.Rating = 99.9
+	fmt.Println("IsClose:", user.Rating)
+
+}
+
+func hello(u User) {
+	fmt.Println("всем привет меня зовут!", u.Name)
+	fmt.Println("мой рейтинг:", u.Rating)
 }
